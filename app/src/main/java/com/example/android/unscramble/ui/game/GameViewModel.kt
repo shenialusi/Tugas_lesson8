c6f0b7f636ece06c6735aa23c3862c7cc738a94c
Reisha23 <esareisha01@gmail.com>
2023-09-27 13:17:54 +0700

    Siklus proses ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -7,7 +7,7 @@ class GameViewModel : ViewModel() {
 
     private var score = 0
     private var currentWordCount = 0
-    private var _currentScrambledWord = "tes"
+    private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
